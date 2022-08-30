4e786bd46a373ffe1560208e28ec5a1df53aa865
Bedwar <31661075+Bedwar@users.noreply.github.com>
2022-08-29 22:17:51 -0300

    Add files via upload

---
+++ src/todo/ToDo.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package todo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author beatr
+ */
+public class ToDo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<String> task = new ArrayList<String>(); 
+        Scanner sc = new Scanner(System.in);
+        int id=0,i=0,op;
+        System.out.println("1 - New Task \n2 - Remove task \n3 - View List");
+        op = sc.nextInt();
+        do{
+        
+        switch (op) {
+  case 1:
+    System.out.println("New task: ");
+     task.add(sc.nextLine());
+    break;
+  case 2:
+      System.out.println("ID task remove: ");
+      task.get(id=sc.nextInt());
+      System.out.println("Remover task "+ task.toString() +"?");
+      String choice=null;
+      if(choice.equals("nao")){
+          System.out.println("Task not removed");
+      }else{
+      task.remove(0);
+      }
+    break;
+  case 3:
+    System.out.println("View List");
+        for (i = 0; i < task.size(); i++) {
+      System.out.println(task.get(i));
+
+ 
+        
+        }
+        
+    break;
+        }
+        } while(op <= 0);
+        
+    }
+    
+}
